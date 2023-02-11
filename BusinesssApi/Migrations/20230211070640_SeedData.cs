d514b07bd4db529e30d279b899653082f9b6338b
Grace Lee <gracelee9901@gmail.com>
2023-02-10 23:07:10 -0800

    Seed database

---
+++ BusinesssApi/Migrations/20230211070640_SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinesssApi.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Businesses",
+                columns: new[] { "BusinessId", "Address", "Description", "Name", "Rating", "Review" },
+                values: new object[,]
+                {
+                    { 1, "123 Moole Street", "Fancy american restaurant", "Starz Restaurant", 4.5999999999999996, "Pricy and need to make reservations" },
+                    { 2, "124 Moole Street", "Antique bookstore", "Rexie's Bookstore", 3.1000000000000001, "Not a lot of books" },
+                    { 3, "125 Moole Street", "Local handmande jewlery shop", "Matilda Jewlers", 5.0, "Has a variety of jewlery and can customize" },
+                    { 4, "126 Moole Street", "Unique ice cream flavor shop", "Pip's Ice Cream", 4.0999999999999996, "Can try all flavors" },
+                    { 5, "127 Moole Street", "Pet Shop", "Over the Sea", 2.5, "Employee don't help out" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 5);
+        }
+    }
+}
--- BusinesssApi/Models/BusinessApiContext.cs
+++ BusinesssApi/Models/BusinessApiContext.cs
@@ -9,5 +9,17 @@ namespace BusinessApi.Models
     public BusinessApiContext(DbContextOptions<BusinessApiContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Business>()
+        .HasData(
+          new Business { BusinessId = 1, Name = "Starz Restaurant", Address = "123 Moole Street", Description = "Fancy american restaurant", Rating = 4.6, Review = "Pricy and need to make reservations" },
+          new Business { BusinessId = 2, Name = "Rexie's Bookstore", Address = "124 Moole Street", Description = "Antique bookstore", Rating = 3.1, Review = "Not a lot of books" },
+          new Business { BusinessId = 3, Name = "Matilda Jewlers", Address = "125 Moole Street", Description = "Local handmande jewlery shop", Rating = 5.0, Review = "Has a variety of jewlery and can customize" },
+          new Business { BusinessId = 4, Name = "Pip's Ice Cream", Address = "126 Moole Street", Description = "Unique ice cream flavor shop", Rating = 4.1, Review = "Can try all flavors" },
+          new Business { BusinessId = 5, Name = "Over the Sea", Address = "127 Moole Street", Description = "Pet Shop", Rating = 2.5, Review = "Employee don't help out" }
+        );
+    }
   }
 }
